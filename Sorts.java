b3248242ab4ca494cf474aab9ac4711c36797df9
Yuqing Wu <ywu20@stuy.edu>
2020-12-16 13:00:39 -0500

    initial commit

---
+++ Sorts.java
@@ -0,0 +1,10 @@
+public class Sorts{
+  /**Bubble sort of an int array.
+  *@postcondition The array will be modified such that the elements will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+
+  public static void BubbleSort(int[] data){
+
+  }
+}
